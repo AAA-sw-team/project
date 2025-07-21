c32d5a7a1da44bd205e912c82eb58ff71d849d49
dubyann <1720634143@qq.com>
2025-07-21 22:26:29 +0800

    修复bug

--- popquiz-app/backend/routers/quizRoutes.js
+++ popquiz-app/backend/routers/quizRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { generateQuiz, getQuizzes, publishQuizzes } = require('../controllers/quizController');
+const { generateQuiz, getQuizzes/*, publishQuizzes*/ } = require('../controllers/quizController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 //生成题目
 router.post('/generate/:lectureId', authMiddleware, generateQuiz);
 // 批量发布题目
-router.post('/publish', authMiddleware, publishQuizzes);
+//router.post('/publish', authMiddleware, publishQuizzes);
 // 获取某讲座已发布的所有题目
 router.get('/:lectureId', getQuizzes);
 
